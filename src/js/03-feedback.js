efa05662a4dedb57b3e8381a175874345984ba49
MaksymMyhulia <113797631+MaksymMyhulia@users.noreply.github.com>
2023-02-28 15:16:01 +0200

    Correct_feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,8 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector(".feedback-form");
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
 
 form.addEventListener("submit", onFormSubmit);
 form.addEventListener("input", throttle(onInputValue, 500));
@@ -9,17 +11,24 @@ function onFormSubmit (e) {
 e.preventDefault();
 const {email, message} = e.currentTarget.elements;
 console.log({email: email.value, message: message.value});
-localStorage.removeItem('feedback-form-state');
+localStorage.removeItem(LOCALSTORAGE_KEY);
 e.currentTarget.reset();
 }
 
 function onInputValue (e) {
-let value = localStorage.getItem('feedback-form-state');
+let value = localStorage.getItem(LOCALSTORAGE_KEY);
 value = value ? JSON.parse(value) : {};
 let {email, message} = form.elements;
 value = {
     email: email.value,
     message: message.value
 }
-localStorage.setItem('feedback-form-state', JSON.stringify(value));
+localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(value));
+}
+
+const localStorageData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+if (localStorageData) {
+    form.email.value = localStorageData.email;
+    form.message.value = localStorageData.message;
 }
